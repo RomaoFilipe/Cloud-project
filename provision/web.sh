7d5c02c9376e06c21ad48d33e7111fe61173ba63
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-06-18 23:40:50 +0100

    web.sh file updated to config only webservers

--- provision/web.sh
+++ provision/web.sh
@@ -8,7 +8,6 @@ echo -e "$MSG_COLOR$(hostname): Install Apache HTTP Server\033[0m"
 sudo apt-get install -y apache2
 
 echo -e "$MSG_COLOR$(hostname): Install PHP 8.1\033[0m"
-# Install specific versions of PHP packages
 sudo apt install -y --no-install-recommends php8.1
 
 echo -e "$MSG_COLOR$(hostname): Install additional PHP 8.1 modules\033[0m"
@@ -27,68 +26,21 @@ sudo apt-get install -y \
     zip \
     unzip
 
-# sudo sh -c 'echo -e "<?php\nphpinfo();\n?>" > /var/www/html/phpinfo.php'
-
 sudo systemctl restart apache2
 
-echo -e "$MSG_COLOR$(hostname): Install PostgreSQL and its PHP extension\033[0m"
-# Install specific version of PostgreSQL
-sudo apt-get install -y postgresql-14 php-pgsql
-
-# Change to /tmp directory since the next commands will run as "postgres" user
-# to avoid could not change directory to "/home/vagrant": Permission denied
-cd /tmp
-
-echo -e "$MSG_COLOR$(hostname): Create a new PostgreSQL user and database\033[0m"
-sudo -u postgres psql -c "CREATE USER myuser WITH PASSWORD 'mypassword';"
-sudo -u postgres psql -c "CREATE DATABASE mydatabase OWNER myuser;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;"
-
-# peer access to myuser
-# sudo sh -c 'echo "local   all             myuser                                  peer" >> /etc/postgresql/14/main/pg_hba.conf'
-sudo service postgresql restart
-
-sudo -u postgres psql -d mydatabase -c "DROP TABLE test;"
-
-echo -e "$MSG_COLOR$(hostname): Import dump.sql and set user privileges\033[0m"
-# PGPASSWORD=mypassword sudo -u postgres psql -U myuser -h localhost -d mydatabase -f /vagrant/provision/dump.sql # change to ./provision/dump.sql
-sudo -u postgres psql -d mydatabase -f /vagrant/provision/dump.sql
-sudo -u postgres psql -d mydatabase -c "GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE messages TO myuser;" # uneeded?
-sudo -u postgres psql -d mydatabase -c "GRANT USAGE, SELECT, UPDATE ON SEQUENCE messages_id_seq TO myuser;"
-sudo -u postgres psql -d mydatabase -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO myuser;"
-sudo -u postgres psql -d mydatabase -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO myuser;"
-sudo -u postgres psql -d mydatabase -c "GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;"
-
-echo -e "$MSG_COLOR$(hostname): View users and databases in PostgreSQL\033[0m"
-sudo -u postgres psql -c "\du"
-sudo -u postgres psql -c "\list"
-sudo -u postgres psql -d mydatabase -c "\dt"
-
-# Change back to the previous folder
-cd -
-
-echo -e "$MSG_COLOR$(hostname): Install Composer (PHP)\033[0m"
-cd ~
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
-
-echo -e "$MSG_COLOR$(hostname): Install dependencies for websockets server\033[0m"
-cd /vagrant/ws
-sudo -u vagrant bash -c 'composer install'
-
-echo -e "$MSG_COLOR$(hostname): Install dependencies for webapp\033[0m"
-cd /vagrant/app
-sudo -u vagrant bash -c 'composer install'
-
-echo -e "$MSG_COLOR$(hostname): Copy apache config, disable the default site / enable ours\033[0m"
+echo -e "$MSG_COLOR$(hostname): Configure Apache\033[0m"
 sudo cp /vagrant/provision/projectA.conf /etc/apache2/sites-available/
 sudo a2dissite 000-default.conf
 sudo a2ensite projectA.conf
 sudo systemctl reload apache2
 
-echo -e "$MSG_COLOR$(hostname): Update deploy date @ .env file\033[0m"
-cd /vagrant/app
-ISO_DATE=$(TZ=Europe/Lisbon date -Iseconds)
-sed -i "s/^DEPLOY_DATE=.*/DEPLOY_DATE=\"$ISO_DATE\"/" .env
+echo -e "$MSG_COLOR$(hostname): Install Composer\033[0m"
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-echo -e "\033[42m$(hostname): Finished! Visit http://192.168.44.10\033[0m"
+echo -e "$MSG_COLOR$(hostname): Install application dependencies\033[0m"
+cd /var/www/html
+sudo -u vagrant composer install
+
+echo -e "\033[42m$(hostname): Web server setup completed\033[0m"
+
