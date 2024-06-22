c265b5cf775035acdad94a8b096923a5f9b3073b
Frederico <aluno24391@ipt.pt>
2024-06-22 23:20:20 +0100

    fixing database.sh

--- provision/database.sh
+++ provision/database.sh
@@ -7,18 +7,19 @@ sudo apt-get update
 echo -e "$MSG_COLOR$(hostname): Install PostgreSQL\033[0m"
 sudo apt-get install -y postgresql-14
 
-# Temporarily set PostgreSQL to trust authentication for local connections
-echo -e "$MSG_COLOR$(hostname): Temporarily set PostgreSQL to trust authentication for local connections\033[0m"
-sudo sed -i "s/local\s*all\s*all\s*peer/local all all trust/" /etc/postgresql/14/main/pg_hba.conf
-sudo sed -i "s/host\s*all\s*all\s*127.0.0.1\/32\s*scram-sha-256/host all all 127.0.0.1\/32 trust/" /etc/postgresql/14/main/pg_hba.conf
-sudo sed -i "s/host\s*all\s*all\s*::1\/128\s*scram-sha-256/host all all ::1\/128 trust/" /etc/postgresql/14/main/pg_hba.conf
-
-sudo service postgresql restart
-
 echo -e "$MSG_COLOR$(hostname): Create a new PostgreSQL user and database\033[0m"
-sudo -u postgres psql -c "CREATE USER myuser WITH PASSWORD 'mypassword';" || true
-sudo -u postgres psql -c "CREATE DATABASE mydatabase OWNER myuser;" || true
+sudo -u postgres psql -c "CREATE USER myuser WITH PASSWORD 'mypassword';"
+sudo -u postgres psql -c "CREATE DATABASE mydatabase OWNER myuser;"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;"
+
+echo -e "$MSG_COLOR$(hostname): Update pg_hba.conf for md5 authentication\033[0m"
+sudo bash -c 'echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/14/main/pg_hba.conf'
+
+echo -e "$MSG_COLOR$(hostname): Update postgresql.conf to listen on all addresses\033[0m"
+sudo sed -i "s/^#listen_addresses = .*/listen_addresses = '*'/" /etc/postgresql/14/main/postgresql.conf
+
+echo -e "$MSG_COLOR$(hostname): Restart PostgreSQL\033[0m"
+sudo systemctl restart postgresql
 
 echo -e "$MSG_COLOR$(hostname): Import dump.sql and set user privileges\033[0m"
 sudo -u postgres psql -d mydatabase -f /vagrant/provision/dump.sql
@@ -27,14 +28,6 @@ sudo -u postgres psql -d mydatabase -c "GRANT USAGE, SELECT, UPDATE ON SEQUENCE 
 sudo -u postgres psql -d mydatabase -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO myuser;"
 sudo -u postgres psql -d mydatabase -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO myuser;"
 sudo -u postgres psql -d mydatabase -c "GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;"
-
-# Revert pg_hba.conf back to md5 authentication
-echo -e "$MSG_COLOR$(hostname): Revert pg_hba.conf back to md5 authentication\033[0m"
-sudo sed -i "s/local\s*all\s*all\s*trust/local all all md5/" /etc/postgresql/14/main/pg_hba.conf
-sudo sed -i "s/host\s*all\s*all\s*127.0.0.1\/32\s*trust/host all all 127.0.0.1\/32 md5/" /etc/postgresql/14/main/pg_hba.conf
-sudo sed -i "s/host\s*all\s*all\s*::1\/128\s*trust/host all all ::1\/128 md5/" /etc/postgresql/14/main/pg_hba.conf
-
-sudo service postgresql restart
 
 echo -e "$MSG_COLOR$(hostname): View users and databases in PostgreSQL\033[0m"
 sudo -u postgres psql -c "\du"
