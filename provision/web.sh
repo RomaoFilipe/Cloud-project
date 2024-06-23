ff60d73944195ee9dbdc21a65736644d245a02e3
Frederico <aluno24391@ipt.pt>
2024-06-23 15:53:47 +0100

    Fixing the registration of webapp servers in Consul services

--- provision/web.sh
+++ provision/web.sh
@@ -53,6 +53,63 @@ sudo bash -c 'echo "session.save_handler = redis" >> /etc/php/8.1/apache2/php.in
 sudo bash -c 'echo "session.save_path = \"tcp://192.168.44.20:6379\"" >> /etc/php/8.1/apache2/php.ini'
 sudo systemctl restart apache2
 
+echo -e "$MSG_COLOR$(hostname): Install Consul\033[0m"
+sudo apt-get install -y consul
+
+# Determine the hostname and configure Consul accordingly
+hostname=$(hostname)
+
+if [[ "$hostname" == "webapp1" ]]; then
+    ip_address="192.168.44.11"
+    service_name="webapp1"
+elif [[ "$hostname" == "webapp2" ]]; then
+    ip_address="192.168.44.12"
+    service_name="webapp2"
+elif [[ "$hostname" == "webapp3" ]]; then
+    ip_address="192.168.44.13"
+    service_name="webapp3"
+else
+    echo -e "$MSG_COLOR$(hostname): Unknown hostname. Exiting...\033[0m"
+    exit 1
+fi
+
+echo -e "$MSG_COLOR$(hostname): Configure Consul for $service_name\033[0m"
+cat <<EOF | sudo tee /etc/consul.d/$service_name.json
+{
+  "service": {
+    "name": "$service_name",
+    "tags": ["web"],
+    "address": "$ip_address",
+    "port": 80,
+    "check": {
+      "http": "http://$ip_address",
+      "interval": "10s"
+    }
+  }
+}
+EOF
+
+# Create a data directory for Consul
+sudo mkdir -p /opt/consul
+
+echo -e "$MSG_COLOR$(hostname): Start Consul Agent\033[0m"
+sudo tee /etc/systemd/system/consul.service <<EOF
+[Unit]
+Description=Consul Agent
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/consul agent -config-dir=/etc/consul.d/ -bind=$ip_address -client=0.0.0.0 -retry-join=192.168.44.15 -data-dir=/opt/consul
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable consul
+sudo systemctl start consul
+
 echo -e "$MSG_COLOR$(hostname): Update deploy date @ .env file\033[0m"
 cd /vagrant/app
 ISO_DATE=$(TZ=Europe/Lisbon date -Iseconds)
