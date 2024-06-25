d0bf3246d7cb2a9f4b9beb0a50a4a4c34427e8d2
Frederico <aluno24391@ipt.pt>
2024-06-25 09:50:52 +0100

    websockets working

--- Cloud-Project-B/app/public_html/websockets.php
+++ Cloud-Project-B/app/public_html/websockets.php
@@ -44,7 +44,7 @@
     <script src="js/bootstrap.bundle.min.js"></script>
     <!-- Load WebSocket script -->
     <script>
-        const ws_uri = "<?php echo 'ws://' . $ws_host . ':' . $ws_port; ?>";
+        const ws_uri = "ws://localhost:8000"; // Ajuste para localhost para teste
     </script>
     <script src="js/websockets.js" defer></script>
 </body>
